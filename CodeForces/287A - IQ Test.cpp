1944b7ccd2de58ebbf631acaa9fcd9cb5047273b
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-09-02 20:02:20 +0530

    287A

---
+++ CodeForces/287A - IQ Test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> vs(4);
+    for (size_t i = 0; i < 4; ++i)
+    {
+        cin >> vs[i];
+    }
+
+    bool pass = false;
+    for (size_t top = 0; top <= 2 && !pass; ++top)
+    {
+        for (size_t left = 0; left <= 2; ++left)
+        {
+            int dot = 0;
+            for (size_t r = 0; r < 2; ++r)
+            {
+                for (size_t c = 0; c < 2; ++c)
+                {
+                    if (vs[top+r][left+c] == '.')
+                    {
+                        dot += 1;
+                    }
+                }
+            }
+            if (dot != 2)
+            {
+                pass = true;
+                break;
+            }
+        }
+    }
+    cout << (pass ? "YES" : "NO") << endl;
+    return 0;
+}
