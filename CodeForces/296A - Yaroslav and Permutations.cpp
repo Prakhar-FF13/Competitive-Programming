190da28a34dba2c925945ee3790bad3d5a9c7b74
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-09-03 18:09:57 +0530

    296A Done myself this time.

--- CodeForces/296A - Yaroslav and Permutations.cpp
+++ CodeForces/296A - Yaroslav and Permutations.cpp
@@ -1,90 +1,24 @@
-#include<set>
-#include<map>
-#include<list>
-#include<iomanip>
-#include<cmath>
-#include<string>
-#include<vector>
-#include<queue>
-#include<stack>
-#include<complex>
-#include<sstream>
-#include<iostream>
-#include<fstream>
-#include<algorithm>
-#include<numeric>
-#include<utility>
-#include<functional>
-#include<stdio.h>
-#include<assert.h>
-#include<memory.h>
-#include<bitset>
+#include<bits/stdc++.h>
 using namespace std;
 
-#define all(v)				((v).begin()), ((v).end())
-#define sz(v)				((int)((v).size()))
-#define clr(v, d)			memset(v, d, sizeof(v))
-#define rep(i, v)		for(int i=0;i<sz(v);++i)
-#define lp(i, n)		for(int i=0;i<(int)(n);++i)
-#define lpi(i, j, n)	for(int i=(j);i<(int)(n);++i)
-#define lpd(i, j, n)	for(int i=(j);i>=(int)(n);--i)
-
-typedef long long         ll;
-const ll OO = 1e8;
-
-const double EPS = (1e-7);
-int dcmp(double x, double y) {	return fabs(x-y) <= EPS ? 0 : x < y ? -1 : 1;	}
-
-#define pb					push_back
-#define MP					make_pair
-#define P(x)				cout<<#x<<" = { "<<x<<" }\n"
-typedef long double   	  ld;
-typedef vector<int>       vi;
-typedef vector<double>    vd;
-typedef vector< vi >      vvi;
-typedef vector< vd >      vvd;
-typedef vector<string>    vs;
-
-
-
-int main()
-{
-#ifndef ONLINE_JUDGE
-	freopen("c.in", "rt", stdin);
-	//freopen(".txt", "wt", stdout);
-#endif
-
-	int arr[100];
-
+int main(){
 	int n;
 	cin>>n;
-
-	for(int i = 0; i < n; ++i)
+	int arr[n];
+	for(int i = 0 ; i < n ; i++){
 		cin>>arr[i];
-
-	bool valid = true;
-
-	//for each number, know how many times repeated
-	for(int i = 0; i < n; ++i)
-	{
-		int C = 0;
-		for(int j = 0; j < n; ++j)
-		{
-			if(arr[i] == arr[j])
-				++C;
-		}
-
-		if(n < 2*C-1)
-		{
-			valid = false;
-			break;
-		}
 	}
+	bool pos=true;
+	for(int i = 0; i < n ; i++){
+		int cnt=0;
+		for(int j = 0 ; j < n ; j++)
+			if(arr[i] == arr[j])
+				cnt++;
 
-
-	if(valid)
-		cout<<"YES\n";
-	else
-		cout<<"NO\n";
+		if(n-cnt < (cnt-1) )
+			pos = false;
+	}
+	if(pos) cout<<"YES";
+	else 	cout<<"NO";
 	return 0;
 }
