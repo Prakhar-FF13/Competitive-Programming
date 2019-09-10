3d111125f265419b4fb5f7e2965eab07dcc101d8
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-09-10 19:11:35 +0530

    Unsigned Long Long is needed for everyting.

---
+++ CodeForces/439B - Devu, the Dumb Guy.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main(){
+    unsigned long long n,x;
+    cin>>n>>x;
+    unsigned long long arr[n];
+    rep(i,0,n)
+        cin>>arr[i];
+    sort(arr, arr+n);
+    unsigned long long ans = 0;
+    rep(i, 0, n){
+        ans += (arr[i]*x);
+        x = (x-1 > 0) ? x-1: 1;
+    }
+    cout<<ans;
+    return 0;
+}
