044fe9911346a752a530e6635019848ac1ec9991
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-10-08 13:46:07 +0530

    535B - BitMasking Good Question Must try

---
+++ CodeForces/535B - Tavas and SaDDas.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main(){
+    string x;
+    cin>>x;
+    int digits = x.length();
+    int numBLessthanX = (1<<digits)-2;
+    ll ans = 1 + numBLessthanX;
+    for(int i = digits-1, pos=0; i>= 0 ; i--, pos++){
+        if(x[i] == '7')
+            ans += (1<<pos);
+    }
+    cout<<ans;
+    return 0;
+}
