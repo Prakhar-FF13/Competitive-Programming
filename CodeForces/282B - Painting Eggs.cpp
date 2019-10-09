10f2ec48ef2d781afd9cffa216da1d0f07f0c8bd
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-10-09 15:21:43 +0530

    282B - Greedy good Question

---
+++ CodeForces/282B - Painting Eggs.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main(){
+    fsi
+    int n;
+    cin>>n;
+    ll s1 = 0,s2 = 0;
+    string str = "";
+    while(n--){
+        int a,g;
+        cin>>a>>g;
+        if(abs(s1+a - s2) <= 500){
+            s1 += a;
+            str += "A";
+        }else{
+            s2 += g;
+            str += "G";
+        }
+    }
+    cout<<str;
+    return 0;
+}
