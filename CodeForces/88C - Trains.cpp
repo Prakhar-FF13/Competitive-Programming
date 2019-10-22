0223dac7d0abfb5f8910d24a9e5f2f610f8d0c69
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-10-22 19:10:08 +0530

    88C - LCM Question

---
+++ CodeForces/88C - Trains.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+ll gcd(ll a, ll b){
+    if(b == 0)
+        return a;
+    return gcd(b, a%b);
+}
+ll lcm(ll a, ll b){
+    return (a*b)/gcd(a, b);
+}
+int main(){
+    ll a,b;
+    cin>>a>>b;
+    ll lc = lcm(a, b);
+    ll ac = lc/a;
+    ll bc = lc/b;
+    if(a < b)
+        ac--;
+    else
+        bc--;
+    if(ac > bc)
+        cout<<"Dasha";
+    else if(bc > ac)
+        cout<<"Masha";
+    else
+        cout<<"Equal";
+    return 0;
+}
