1d35bfdd807f7685f3f5bdcaacbdaca41683f994
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-10-27 18:43:03 +0530

    Minimum Good Permutations Easy

---
+++ Codechef/Minimum Good Permutation - MINPERM.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main(){
+    tt(){
+        int n;
+        cin>>n;
+        int arr[n+1];
+        rep(i, 0, n+1)
+            arr[i] = i;
+        for(int i = 1; i < n ; i += 2)
+            swap(arr[i], arr[i+1]);
+        if(n%2 == 1)
+            swap(arr[n], arr[n-1]);
+        rep(i, 1, n+1)
+            cout<<arr[i]<<" ";
+        cout<<endl;
+    }
+    return 0;
+}
