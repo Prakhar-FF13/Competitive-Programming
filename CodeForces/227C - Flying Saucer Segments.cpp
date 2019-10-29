0e4c77c73349e8b0f6b4806ecf524e17b4cd09da
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-10-29 19:45:23 +0530

    recursively Define problem, generate Fn interms of Fn-1 Good Question

---
+++ CodeForces/227C - Flying Saucer Segments.cpp
@@ -0,0 +1,62 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+ll powerMod(ll n, ll p, ll m){
+    ll res = 1;
+    while(p){
+        if(p&1)
+            res = (res * n)%m;
+        n = (n * n)%m;
+        p = p>>1;
+    }
+    return res;
+}
+
+int main(){
+    ll n,m;
+    cin>>n>>m;
+    ll ans = powerMod(3, n, m);
+    ans--;
+    if(ans < 0) ans += m;
+    cout<<ans;
+    return 0;
+}
