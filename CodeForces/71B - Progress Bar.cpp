45d4c0afad19dc2d88b8001d47ee6152aada9ad9
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-10-31 17:06:28 +0530

    71B - Progress Bar Easy

---
+++ CodeForces/71B - Progress Bar.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main(){
+    int n,k,t;
+    cin>>n>>k>>t;
+    int z = (n*k*t*1.0) / 100.0;
+    while(z >= k){
+        cout<<k<<" ";
+        z -= k;
+        n--;
+    }
+    if(z)
+        n--, cout<<z<<" ";
+    rep(i, 0, n)
+        cout<<0<<" ";
+    return 0;
+}
