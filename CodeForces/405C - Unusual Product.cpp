517b94ad499150d67482ecbeadce63affa72bbac
Prakhar-FF13 <prakhar.meerut@gmail.com>
2019-11-17 15:27:28 +0530

    405C - Observe

---
+++ CodeForces/405C - Unusual Product.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main(){
+    RW()
+    int n;
+    scanf("%d", &n);
+    vi diagonals(n+1);
+    int sum = 0;
+    rep(i, 0, n)
+        rep(j, 0, n){
+            int x;scanf("%d", &x); if(i==j)  diagonals[i+1] = x, sum += x;
+        }
+    tt(){
+        int type;
+        scanf("%d", &type);
+        if(type==3)
+            printf("%d", sum%2);
+        else{
+            int numb;
+            scanf("%d", &numb);
+            sum -= diagonals[numb];
+            diagonals[numb] = 1 - diagonals[numb];
+            sum += diagonals[numb];
+        }
+    }
+    return 0;
+}
