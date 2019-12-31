39d084659c04dbca046d5875d6e00c177f551515
Prakhar-FF13 <prakhar.meerut@gmail.com>
2020-01-01 00:21:47 +0530

    735C - Tennis championship DP Fibonacci

---
+++ CodeForces/735C - Tennis championship.cpp
@@ -0,0 +1,63 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+// solution in goodnotes 5
+
+ll dp[100];
+
+int main(){
+    ll n;
+    cin>>n;
+    dp[0] = 1;
+    dp[1] = 2;
+    dp[2] = 3;
+    ll ans;
+    rep(i, 3, 100)
+        dp[i] = dp[i-1] + dp[i-2];
+    for(int i = 1 ; i < 100 ; i++){
+        if(dp[i] <= n)  ans = i;
+        else            break;
+    }
+    cout<<ans;
+    return 0;
+}
