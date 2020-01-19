5e23fb797fa5cc204295877a9276518da65221cf
Prakhar-FF13 <prakhar.meerut@gmail.com>
2020-01-19 17:14:37 +0530

    979B - Handles All Cases

--- CodeForces/979B - Treasure Hunt.cpp
+++ CodeForces/979B - Treasure Hunt.cpp
@@ -1,96 +1,33 @@
-#include<iostream>
-#include<cstdio>
-#include<string.h>
-#include<climits>
-#include<sstream>
-#include<vector>
-#include<list>
-#include<stack>
-#include<queue>
-#include<set>
-#include<map>
-#include<bitset>
-#include<unordered_map>
-#include<unordered_set>
-#include<cmath>
-#include<algorithm>
-#include<numeric>
-#include<functional>
+#include <bits/stdc++.h>
 
 using namespace std;
-typedef long long ll;
-typedef pair<int,int> pii;
-typedef pair<ll,ll> pll;
-typedef vector<int> vi;
-typedef vector<ll> vl;
-typedef vector<double> vd;
-typedef vector<pii> vii;
-typedef vector<pll> vll;
-typedef pair<double, pii> pdii;
-typedef pair<double, pll> pdll;
-typedef vector<pdii> vdii;
-typedef vector<pdll> vdll;
-#define rep(i,a,b)		for(int i=a;i<b;i++)
-#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
-#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
-#define MOD 	1000000007
-#define tt() 	int t;cin>>t;while(t--)
-#define pb		push_back
-#define mp		make_pair
-#define ms		memset
-#define all(v)		v.begin(),v.end()
-#define pob		pop_back
-int charCount[26*2+3];
-string a,b,c;
-int n;
-void countChar(string str){
-    rep(i ,0, 26*2+3)
-        charCount[i] = 0;
-    rep(i, 0, str.length()){
-        if(str[i] >= 'A' && str[i] <= 'Z'){
-            int ind = str[i] - 'A';
-            charCount[ind]++;
-        }
-        else{
-            int ind = str[i] - 'a';
-            charCount[ind+27]++;
-        }
-    }
-}
 
-int maxBeauty(string str, int t){
-    int ans = 0;
-    countChar(str);
-    rep(i, 0, 26*2+3){
-        int maxCount = charCount[i], turns = t, subAns = 0;
-        if(maxCount + turns <= str.length()){
-            subAns = maxCount + turns;
-            turns = 0;
-        }else{
-            turns = turns - str.length() + maxCount;
-            subAns = str.length() - (turns%2);
-        }
-        ans = max(ans, subAns);
-    }
-    return ans;
-}
+int a[256], b[256], c[256], n, ma, mb, mc;
+string p, q, r;
 
-int main(){
-    cin>>n;
-    cin>>a>>b>>c;
-    int bA = maxBeauty(a, n);
-    int bB = maxBeauty(b, n);
-    int bC = maxBeauty(c, n);
-    int maxB = max(bA, max( bB, bC ));
-    int countMaxB = 0;
-    if(maxB == bA) countMaxB++;
-    if(maxB == bB) countMaxB++;
-    if(maxB == bC) countMaxB++;
-    if(countMaxB >= 2)
-        cout<<"Draw";
-    else{
-        if(maxB == bA)          cout<<"Kuro";
-        else if(maxB == bB)     cout<<"Shiro";
-        else                    cout<<"Katie";
-    }
+int main() {
+      cin >> n >> p >> q >> r;
+      for (char x: p) ma = max(ma, ++a[x]);
+      for (char x: q) mb = max(mb, ++b[x]);
+      for (char x: r) mc = max(mc, ++c[x]);
+      if (n == 1 && ma == (int)p.length()) p.pop_back();
+      if (n == 1 && mb == (int)q.length()) q.pop_back();
+      if (n == 1 && mc == (int)r.length()) r.pop_back();
+      ma = min(ma + n, (int)p.length());
+      mb = min(mb + n, (int)q.length());
+      mc = min(mc + n, (int)r.length());
+      if (ma > mb && ma > mc) {
+            puts("Kuro");
+            return 0;
+      }
+      if (mb > ma && mb > mc) {
+            puts("Shiro");
+            return 0;
+      }
+      if (mc > ma && mc > mb) {
+            puts("Katie");
+            return 0;
+      }
+      puts("Draw");
+      return 0;
 }
