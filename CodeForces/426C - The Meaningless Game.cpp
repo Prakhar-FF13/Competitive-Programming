8bc51e7f08d326b63e0af26273fc73c6c91abec7
Prakhar-FF13 <prakhar.meerut@gmail.com>
2020-04-05 15:44:24 +0530

    426C Maths problem good

---
+++ CodeForces/426C - The Meaningless Game.cpp
@@ -0,0 +1,79 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;while(t--)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+ll cubic_root (ll num) {
+  ll ans = 0;
+  ll low = 1, high = 1e6+100;
+  while(low <= high) {
+    ll mid = (low + high) / 2, t;
+    t = mid * mid * mid;
+    if (t == num) {
+      ans = mid;
+      break;
+    }else if (t > num) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return ans;
+}
+
+int main(){
+  fsi
+  int n;
+  scanf("%d", &n);
+  while(n--) {
+    ll a, b;
+    scanf("%lld %lld", &a, &b);
+    ll cbr = cubic_root(a*b);
+    if ( cbr && a%cbr == 0 && b%cbr == 0 ) {
+      puts("Yes");
+    } else {
+      puts("No");
+    }
+  }
+  return 0;
+}
