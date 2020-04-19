2a29716433eea5db9b9d20dc6a846ef400690962
Prakhar-FF13 <prakhar.meerut@gmail.com>
2020-04-19 14:13:45 +0530

    Easy greedy

---
+++ CodeForces/166C - Secret.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;while(t--)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+int main(){
+  int n,k;
+  cin>>n>>k;
+  if (3*k > n){
+    cout<<-1;
+    return 0;
+  }
+  int m = 1;
+  for(int i = 1 ; i <= k ; i++) {
+    cout<<m++<<" "; 
+  }
+  for(int i = 1 ; i <= k ; i++) {
+    cout<<i<<" "<<i<<" ";
+    m+=2;
+  }
+  while(m <= n){
+    cout<<1<<" ";
+    m++;
+  }
+  return 0;
+}
