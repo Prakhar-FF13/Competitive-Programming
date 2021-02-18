7aaf4ab8d4dd9f4016759732190c060e5db80aa9
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-02-18 21:19:21 +0530

    Bit manipulation, pre calculation

---
+++ UVA Online Judge/12571 - Brother & Sisters.cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;while(t--)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+int main () {
+    RW()
+    fsi
+    tt() {
+        int n,q;
+        cin>>n>>q;
+        int arr[n];
+        rep(i, 0, n)    cin>>arr[i];
+        int x[230];
+        rep(i, 0, 230) {
+            x[i] = 0;
+            rep(j, 0, n)
+                x[i] = max(x[i], i & arr[j]);
+        }
+        while(q--) {
+            int a;
+            cin>>a;
+            cout<<x[a]<<endl;
+        }
+    }
+}
