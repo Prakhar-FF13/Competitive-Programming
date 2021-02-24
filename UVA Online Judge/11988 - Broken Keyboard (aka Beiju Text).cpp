efc0ac86ed1d075bdf5a6d46af5212bbc0033711
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-02-24 16:59:10 +0530

    Good deque Question

---
+++ UVA Online Judge/11988 - Broken Keyboard (aka Beiju Text).cpp
@@ -0,0 +1,67 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;while(t--)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+int main () {
+    RW()
+    fsi
+    string str;
+    while(cin>>str) {
+        deque<char> d;
+        auto it = d.begin();
+        rep(i, 0, str.length()) {
+            if (str[i] == '[')  {it = d.begin(); continue;}
+            if (str[i] == ']')  {it = d.end(); continue;}
+            it = d.insert(it, str[i]);
+            it++;
+            
+        }
+        for(it = d.begin(); it != d.end(); it++) {
+            cout<<*(it);
+        }
+        cout<<endl;
+    }
+    return 0;
+}
