8857d3446231681fc1682dfb714a112cbfe1f7c2
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-02-27 14:49:47 +0530

    Very Easy map problem

---
+++ UVA Online Judge/12626 - I love pizza.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define fsi		ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	1000000007
+#define tt() 	int t;cin>>t;while(t--)
+#define pb		push_back
+#define mp		make_pair
+#define ms		memset
+#define all(v)		v.begin(),v.end()
+#define pob		pop_back
+
+int main () {
+    RW()
+    fsi
+    tt() {
+        string str;
+        cin>>str;
+        unordered_map<char, int> m;
+        rep(i, 0, str.length()) m[str[i]]++;
+        int ans = MOD;
+        ans = min(ans, m['M']);
+        ans = min(ans, m['A']/3);
+        ans = min(ans, m['R']/2);
+        ans = min(ans, m['G']);
+        ans = min(ans, m['I']);
+        ans = min(ans, m['T']);
+        cout<<ans<<endl;
+    }
+    return 0;
+}
