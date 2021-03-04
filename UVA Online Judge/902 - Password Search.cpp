e7e8d01ec187404f396d8dd3b65f2c683c4f3000
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-03-04 14:27:23 +0530

    Hashmap very easy

---
+++ UVA Online Judge/902 - Password Search.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;while(t--)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+int main () {
+    RW()
+    fsi
+    int n;
+    string str;
+    while(cin>>n>>str) {
+        unordered_map<string, int> m;
+        int len = str.length();
+        int i = 0;
+        while(i+n-1 < len)    m[str.substr(i++, n)]++;
+        int maxC = 0;
+        string ans = "";
+        for(auto it = m.begin(); it != m.end(); it++)
+            if (it->second > maxC)
+                maxC = it->second, ans = it->first;
+        cout<<ans<<endl;
+    }
+    return 0;
+}
