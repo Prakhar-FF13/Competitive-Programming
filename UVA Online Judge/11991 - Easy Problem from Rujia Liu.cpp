aa67ebd33433325fe84f934256ca4a5a35d19626
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-03-14 15:10:12 +0530

    OK problem - hashing + map

---
+++ UVA Online Judge/11991 - Easy Problem from Rujia Liu.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define repit(a)      for(auto it = a.begin(); it != a.end(); it++)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;while(t--)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+int main () {
+    RW()
+    fsi
+    int n,m;
+    while(cin>>n>>m)    {
+        unordered_map<int, vector<int> > indx;
+        rep(i, 1, n+1) {
+            int x;
+            cin>>x;
+            indx[x].pb(i);
+        }
+        rep(i, 0, m) {
+            int k,v;
+            cin>>k>>v;
+            if (k-1 >= indx[v].size())
+                cout<<0<<endl;
+            else
+                cout<<indx[v][k-1]<<endl;
+        }
+    }
+    return 0;
+}
