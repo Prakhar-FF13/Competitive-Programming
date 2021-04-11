e6bbe4910eb7391857252cc5fa2e3928715eeaa2
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-04-11 18:07:24 +0530

    Very Easy Sorting

---
+++ UVA Online Judge/11900 - Boiled Eggs.cpp
@@ -0,0 +1,68 @@
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define repit(a)      for(auto it = a.begin(); it != a.end(); it++)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;for(int xt = 1; xt <= t; xt++)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+int main () {
+    RW()
+    fsi
+
+    int n,p,q;
+    tt() {
+        cin>>n>>p>>q;
+        vector<int> eggs(n);
+        rep(i, 0, n)    cin>>eggs[i];
+        int ans = 0, i = 0, wt = 0;
+        while(i < n && ans < p && wt < q) {
+            if (eggs[i] + wt <= q) ans++, wt += eggs[i];
+            else                    break;
+
+            i++;
+        }
+        cout<<"Case "<<xt<<": "<<ans<<endl;
+    }
+
+    return 0;
+}
