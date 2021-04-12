acf54999639956b2b448f463c14d538b11a15070
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-04-12 14:15:01 +0530

    Good sorting problem

---
+++ UVA Online Judge/10026 - Shoemakers Problem.cpp
@@ -0,0 +1,84 @@
+/*
+    Tags - Good Sorting Problem.
+*/
+
+#include<iostream>
+#include<cstdio>
+#include<string.h>
+#include<climits>
+#include<sstream>
+#include<vector>
+#include<list>
+#include<stack>
+#include<queue>
+#include<set>
+#include<map>
+#include<bitset>
+#include<unordered_map>
+#include<unordered_set>
+#include<cmath>
+#include<algorithm>
+#include<numeric>
+#include<functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i,a,b)		for(int i=a;i<b;i++)
+#define rrep(i,a,b)   for(int i=a;i>b;i--)
+#define repit(a)      for(auto it = a.begin(); it != a.end(); it++)
+#define fsi		        ios_base::sync_with_stdio(false);cin.tie(0);
+#define RW() 		    freopen("read.txt","r",stdin);freopen("write.txt","w",stdout);
+#define MOD 	        1000000007
+#define tt() 	        int t;cin>>t;for(int xt = 1; xt <= t; xt++)
+#define pb		        push_back
+#define mp		        make_pair
+#define mt              make_tuple
+#define ms		        memset
+#define get(t,ind)      get<ind>(t)
+#define all(v)	        v.begin(),v.end()
+#define pob		        pop_back
+
+bool cmp(pair<int, pii> x, pair<int, pii> y) {
+    ll fineIfXFirst = y.first * 1LL * x.second.first;
+    ll fineIfYFirst = x.first * 1LL * y.second.first;
+
+    if (fineIfXFirst == fineIfYFirst)   return x.second.second < y.second.second;
+
+    return fineIfXFirst < fineIfYFirst;
+}
+
+int main () {
+    RW()
+    fsi
+
+    tt() {
+        int n;  cin>>n;
+        vector<pair<int, pii> > arr(n);
+        rep(i, 0, n) {
+            int x, y;
+            cin>>x>>y;
+            arr[i] = mp(y, mp(x, i+1));
+        }
+        sort(all(arr), cmp);
+
+        rep(i, 0, n) {
+            cout<<arr[i].second.second;
+            if (i != n-1)   cout<<" ";
+        }
+        cout<<endl;
+        if (xt != t)    cout<<endl;
+    }
+
+    return 0;
+}
