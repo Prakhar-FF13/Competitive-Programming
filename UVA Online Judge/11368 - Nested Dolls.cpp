c9082897a131cb1092f93cddce1c27df208a5ad1
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-06-03 13:22:26 +0530

    Good problem

---
+++ UVA Online Judge/11368 - Nested Dolls.cpp
@@ -0,0 +1,111 @@
+/*
+  Tags - DP LIS
+    Very good question.
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define pob pop_back
+
+bool cmp(const pii &x, const pii &y)
+{
+  return (x.first < y.first && x.second < y.second);
+}
+
+bool sortCmp(const pii &x, const pii &y)
+{
+  if (x.first == y.first)
+    return x.second > y.second;
+  else
+    return x.first < y.first;
+}
+
+int main()
+{
+  RW()
+  fsi
+
+  tt()
+  {
+    int n;
+    cin >> n;
+    vii arr(n);
+    rep(i, 0, n)
+    {
+      int w, h;
+      cin >> w >> h;
+      arr[i] = mp(w, h);
+    }
+    sort(all(arr), sortCmp);
+
+    deque<pii> L;
+    rep(i, 0, n)
+    {
+      int position = lower_bound(L.begin(), L.end(), arr[i], cmp) - L.begin();
+
+      if (position == 0) // cant merge with any other doll currently.
+      {
+        L.push_front(arr[i]);
+      }
+      else // possible merge with ball smaller than this.
+      {
+        // (position - 1)th  ball can be merged with this so new ball at position-1 is this one.
+        L[position - 1] = arr[i];
+      }
+    }
+
+    cout << L.size() << endl;
+  }
+
+  return 0;
+}
