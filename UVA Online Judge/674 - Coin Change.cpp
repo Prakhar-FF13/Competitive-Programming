3d2a4086e03b598607c506516dae08d3768e5f24
Prakhar Kumar <prakhar.meerut@gmail.com>
2021-09-08 21:08:39 +0530

    Coin Change No duplicates

---
+++ UVA Online Judge/674 - Coin Change.cpp
@@ -0,0 +1,97 @@
+/*
+  Coin change no duplicates - good problem.
+*/
+
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define pob pop_back
+
+ll coins[5];
+ll dp[10000][5];
+
+ll solve(ll w, int idx)
+{
+  if (w == 0)
+    return 1;
+  if (dp[w][idx] != -1)
+    return dp[w][idx];
+  if (w < 0 || idx >= 5)
+    return 0;
+
+  ll ans = 0;
+  for (int i = idx; i < 5; i++)
+  {
+    if (w - coins[i] >= 0)
+      ans += solve(w - coins[i], i);
+  }
+
+  return dp[w][idx] = ans;
+}
+
+int main()
+{
+  RW()
+  fsi;
+  ll w;
+  coins[0] = 1;
+  coins[1] = 5;
+  coins[2] = 10;
+  coins[3] = 25;
+  coins[4] = 50;
+  ms(dp, -1, sizeof(dp));
+  while (cin >> w)
+  {
+    cout << solve(w, 0) << endl;
+  }
+  return 0;
+}
