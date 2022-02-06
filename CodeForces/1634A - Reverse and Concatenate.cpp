9603a1e627f0a1cedf1cf4bbd7c381039b372f71
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-02-06 22:12:02 +0530

    Round 770

---
+++ CodeForces/1634A - Reverse and Concatenate.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x &(-x)
+#define pob pop_back
+
+int main()
+{
+  int t;
+  cin >> t;
+  while (t--)
+  {
+    int n, k;
+    cin >> n >> k;
+    string s;
+    cin >> s;
+    if (k == 0 || n == 1)
+    {
+      cout << 1 << endl;
+    }
+    else
+    {
+      bool alreadyPalindrome = true;
+      int i = 0, j = n - 1;
+      while (i <= j)
+      {
+        if (s[i] != s[j])
+          alreadyPalindrome = false;
+        i++, j--;
+      }
+
+      if (alreadyPalindrome == true)
+        cout << 1 << endl;
+      else
+        cout << 2 << endl;
+    }
+  }
+  return 0;
+}
