a4d14fd0bc5d3b8c577edb49227035971f2bb4c0
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-05-14 15:24:22 +0530

    DFS + Connected Components

---
+++ CSES/Graphs/Building Roads.cpp
@@ -0,0 +1,95 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x &(-x)
+#define pob pop_back
+
+int n, m;
+
+vi g[int(1e5) + 3];
+vi visited;
+
+void dfs(int x)
+{
+  visited[x] = 1;
+  rep(i, 0, g[x].size()) if (visited[g[x][i]] == 0)
+      dfs(g[x][i]);
+}
+
+int main()
+{
+  cin >> n >> m;
+  rep(i, 0, m)
+  {
+    int x, y;
+    cin >> x >> y;
+    g[x].pb(y);
+    g[y].pb(x);
+  }
+  visited.assign(n + 2, 0);
+  vi rep;
+  rep(i, 1, n + 1)
+  {
+    if (visited[i] == 0)
+    {
+      rep.pb(i);
+      dfs(i);
+    }
+  }
+  cout << rep.size() - 1 << endl;
+  rep(i, 1, rep.size())
+  {
+    cout << rep[i - 1] << " " << rep[i] << endl;
+  }
+  return 0;
+}
