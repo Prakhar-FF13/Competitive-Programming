bc2344853f09af4d05a521015fc10f387347c671
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-09-08 16:46:27 +0530

    Good binary search problem

---
+++ CSES/AdditionalProblems/Multiplication Table.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x &(-x)
+#define pob pop_back
+
+ll n;
+
+ll getRank(ll x)
+{
+  ll rank = 0;
+  for (ll i = 1; i <= n; i++)
+    rank += min(n, x / i);
+
+  return rank;
+}
+
+int main()
+{
+  cin >> n;
+  ll low = 1, high = n * n;
+  ll k = ((n * n) / 2) + 1;
+  while (low <= high)
+  {
+    ll mid = low + (high - low) / 2;
+    ll rank = getRank(mid);
+    if (rank < k)
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  cout << low;
+  return 0;
+}
