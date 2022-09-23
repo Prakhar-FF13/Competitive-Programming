2fe3ebb770cd89417a08636fdc73c8a45e7e0a9d
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-09-23 18:56:53 +0530

    Two pointers good problem

---
+++ CodeForces/1251C - Minimize the Integer.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x &(-x)
+#define pob pop_back
+
+int main()
+{
+  RW();
+  tt()
+  {
+    string x;
+    cin >> x;
+    int n = x.length();
+    string a = "", b = "";
+    rep(i, 0, n) if ((x[i] - '0') % 2 == 0) a += x[i];
+    else b += x[i];
+    int i = 0, j = 0;
+    string ans = "";
+    while (i < a.length() && j < b.length())
+    {
+      if (a[i] <= b[j])
+        ans += a[i++];
+      else
+        ans += b[j++];
+    }
+    while (i < a.length())
+      ans += a[i++];
+    while (j < b.length())
+      ans += b[j++];
+    cout << ans << endl;
+  }
+  return 0;
+}
