cfcba0f1372fb22420b6c14392ec1ed559ca8fdd
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-12-06 20:06:03 +0530

    Missing Coin Sum good problem observation

---
+++ CSES/SortingAndSearching/Missing Coin Sum.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x & (-x)
+#define pob pop_back
+
+int main()
+{
+  int n;
+  cin >> n;
+  vector<int> arr(n);
+  rep(i, 0, n) cin >> arr[i];
+  sort(all(arr));
+  long long next = 1;
+  long long cur = 0;
+  for (int i = 0; i < arr.size(); i++)
+  {
+    if (arr[i] > next)
+      break;
+    cur += arr[i];
+    next = cur + 1;
+  }
+  cout << next;
+  return 0;
+}
