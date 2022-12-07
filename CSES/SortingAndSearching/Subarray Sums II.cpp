47684ae37bcf6a3c187a9b649218711c21039d08
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-12-07 17:47:53 +0530

    Subarrays with sum x

---
+++ CSES/SortingAndSearching/Subarray Sums II.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x & (-x)
+#define pob pop_back
+
+int main()
+{
+  ll n, x;
+  cin >> n >> x;
+  vector<ll> arr(n);
+  rep(i, 0, n)
+  {
+    cin >> arr[i];
+    if (i)
+      arr[i] += arr[i - 1];
+  }
+  map<ll, ll> cnt;
+  ll ans = 0;
+  cnt[0] = 1;
+  rep(i, 0, n)
+  {
+    ll req = cnt[arr[i] - x];
+    ans += req;
+    cnt[arr[i]] += 1;
+  }
+  cout << ans << endl;
+  return 0;
+}
