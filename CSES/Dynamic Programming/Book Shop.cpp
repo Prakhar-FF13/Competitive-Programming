4db2f1683d38344f14798d6d8ff4140f2be52900
Prakhar Kumar <prakhar.meerut@gmail.com>
2022-12-14 15:13:54 +0530

    Reaaranging Files

---
+++ CSES/Dynamic Programming/Book Shop.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+#include <cstdio>
+#include <string.h>
+#include <climits>
+#include <sstream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <queue>
+#include <set>
+#include <map>
+#include <bitset>
+#include <unordered_map>
+#include <unordered_set>
+#include <cmath>
+#include <algorithm>
+#include <numeric>
+#include <functional>
+#include <iomanip>
+
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<double> vd;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef pair<double, pii> pdii;
+typedef pair<double, pll> pdll;
+typedef vector<pdii> vdii;
+typedef vector<pdll> vdll;
+#define rep(i, a, b) for (int i = a; i < b; i++)
+#define rrep(i, a, b) for (int i = a; i > b; i--)
+#define repit(a) for (auto it = a.begin(); it != a.end(); it++)
+#define fsi                         \
+  ios_base::sync_with_stdio(false); \
+  cin.tie(0);
+#define RW()                       \
+  freopen("read.txt", "r", stdin); \
+  freopen("write.txt", "w", stdout);
+#define MOD 1000000007
+#define tt() \
+  int t;     \
+  cin >> t;  \
+  for (int xt = 1; xt <= t; xt++)
+#define pb push_back
+#define mp make_pair
+#define mt make_tuple
+#define ms memset
+#define get(t, ind) get<ind>(t)
+#define all(v) v.begin(), v.end()
+#define LSOne(x) x & (-x)
+#define pob pop_back
+
+int main()
+{
+  int n, x;
+  cin >> n >> x;
+  vector<int> pr(n), p(n);
+  rep(i, 0, n) cin >> pr[i];
+  rep(i, 0, n) cin >> p[i];
+  vector<vector<int> > dp(n + 1, vector<int>(x + 1, 0));
+  rep(i, 1, n+1)
+  {
+    rep(j, 1, x+1)
+    {
+      dp[i][j] = dp[i - 1][j];
+      if (j - pr[i - 1] >= 0)
+        dp[i][j] = max(dp[i][j], dp[i - 1][j - pr[i - 1]] + p[i - 1]);
+    }
+  }
+  cout << dp[n][x];
+  return 0;
+}
